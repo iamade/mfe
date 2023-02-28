68f76af368992a2375681809f770d3bd8325a86b
Adesegun Koiki <kamar3deen@gmail.com>
2023-02-28 10:22:34 +0100

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const ModuleFdertionPlugin = require('webpack/lib/container/ModuleFederationPlugin'); 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
@@ -20,9 +20,7 @@ const devConfig = {
             },
             shared: packageJson.dependencies,
         }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
+      
     ],
 };
 
